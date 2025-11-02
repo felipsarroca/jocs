5082b76f49277c044a1decaef2938b5d386f1a66
felipsarroca <felip.sarroca@gmail.com>
2025-11-02 07:14:40 +0100

    .

--- Simon/sw.js
+++ Simon/sw.js
@@ -4,6 +4,7 @@ const urlsToCache = [
   'index.html',
   'styles.css',
   'app.js',
+  'favicon.png',
   'https://cdnjs.cloudflare.com/ajax/libs/tone/14.7.77/Tone.js',
   'https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap',
   'CC_BY-NC-SA.svg',
